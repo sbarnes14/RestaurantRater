b9748e2947cadb228fca245f9067fb0b3088129c
sbarnes14 <sbarnes2015@gmail.com>
2021-06-21 11:41:43 -0400

    Edit RestaurantController and index file

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static RestaurantRater.Models.Restaurant;
 
 namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+
         // GET: Restaurant
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Restaurants.ToList());
         }
     }
 }
